4a4c170af5a0957d456ab40f3bbc746f7ff5ed73
crystallewisdeveloper <107262091+crystallewisdeveloper@users.noreply.github.com>
2024-05-30 10:29:51 -0400

    s7

--- pages/api/deleteUser.js
+++ pages/api/deleteUser.js
@@ -6,7 +6,6 @@ dotenv.config();
 
 const app = express();
 
-
 // CORS middleware
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', 'https://firststep-46e83b.webflow.io');
@@ -33,7 +32,12 @@ const options = {
   onProxyReq: (proxyReq, req, res) => {
     // Debug: Log the target and headers to verify
     console.log(`Proxying request to: ${options.target}`);
+    console.log('Request headers:', req.headers);
     proxyReq.setHeader('Authorization', `Bearer ${process.env.WEBFLOW_API_TOKEN}`);
+  },
+  onProxyRes: (proxyRes, req, res) => {
+    // Debug: Log the response headers
+    console.log('Response headers:', proxyRes.headers);
   }
 };
 
@@ -45,14 +49,6 @@ const apiProxy = createProxyMiddleware(options);
 
 // Use the proxy middleware
 app.use('/api', apiProxy);
-
-// Handle preflight requests
-app.options('*', (req, res) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://firststep-46e83b.webflow.io');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, DELETE, OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.status(200).send();
-});
 
 const PORT = process.env.PORT || 1234;
 app.listen(PORT, () => {
